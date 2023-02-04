43fdc4985bbcddef3251524eb78171b2502265bd
ellzmarie <elliematutisdigal@gmail.com>
2023-02-04 11:43:09 -0800

    first commit

---
+++ functions_lab_deliverable.py
@@ -0,0 +1,53 @@
+# 1. Write a function named sum_to that accepts a single integer, n, and returns the sum of the integers from 1 to n.
+
+# sum_to(6)  # returns 21
+# sum_to(10) # returns 55
+
+
+def sum_to(n):
+    return sum(range(1, n+1))
+
+print(sum_to(6))
+print(sum_to(10))
+
+# 2. Write a function named largest that takes a list of numbers as an argument and returns the largest number in that list.
+
+# largest([1, 2, 3, 4, 0])  # returns 4
+# largest([10, 4, 2, 231, 91, 54])  # returns 231
+
+def largest_number(number):
+    
+    large_number = 0
+    
+    for nums in number:
+        if nums >= max(number):
+           large_number = nums 
+    return large_number
+
+print(largest_number([1, 2, 3, 4, 0]))
+print(largest_number([10, 4, 2, 231, 91, 54]))
+
+# 3. Write a function named occurances that takes two string arguments as input and counts the number of occurances of the second string inside the first string.
+
+# occurances('fleep floop', 'e')   # returns 2
+# occurances('fleep floop', 'p')   # returns 2
+# occurances('fleep floop', 'ee')  # returns 1
+# occurances('fleep floop', 'fe')  # returns 0
+
+def occurances(str1, str2):
+    strings = 0
+    for str in str1.count:
+        if str2 in str1:
+            strings = str2
+    return strings
+
+print(occurances('fleep floop', 'e'))
+
+# Write a function named productthat takes an arbitrary number of numbers, multiplies them all together, and returns the product.
+# (HINT: Review your notes on *args).
+
+# For example:
+
+# product(-1, 4) # returns -4
+# product(2, 5, 5) # returns 50
+# product(4, 0.5, 5) # returns 10.0
